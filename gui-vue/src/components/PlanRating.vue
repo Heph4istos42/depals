7bd376b47d5338fca721919c8364d44a1f3b198c
Jannis Otto <jannis.otto@itc-studenten.de>
2022-07-05 20:44:16 +0200

    fuck

---
+++ gui-vue/src/components/PlanRating.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="rating">
+    
+    <table border="0"> 
+        <tr>
+            <th>
+                <ul class="list">
+                    <star-rating @update:rating ="setRating"></star-rating>
+                </ul>
+            </th>
+        </tr>
+            <tr>
+                <th>How would you f****** rate your f****** plan</th>
+            </tr>
+            <tr>
+                <th> 
+                <textarea v-model="message" placeholder="Your comment"></textarea>
+            </th>
+            </tr>
+            <tr>
+                <th> 
+                <td><button class="btnRate"> SAVE </button></td>
+            </th>
+            </tr>
+        </table>
+  </div>
+</template>
+
+<script>
+  import StarRating from 'vue-star-rating';
+
+export default {
+    components: { StarRating},
+
+    methods: {
+        setRating(rating){
+        this.rating= rating;
+        }
+    },
+    data() {
+        return{rating: 0}
+    }
+}
+</script>
+
+
+<style>
+    .btnActivate {
+        height: 50px;
+        width: 250px;
+        margin-left: 3%;
+        margin-top: 5px;
+    }
+
+    .btnRate {
+        height: 50px;
+        width: 340px;
+        margin-top: 5px;
+    }
+    .plan {
+        width: 100%;
+        text-align: center;
+        justify-content: center;
+        display: flex;
+    }
+    #kreis-green {
+    background-color: #7CFC00;
+    width: 35px;
+    height: 35px;
+    -moz-border-radius: 20px;
+    -webkit-border-radius: 20px;
+	border-radius: 20px;
+    }
+    #kreis-yellow {
+        background-color: #FFFF00;
+        width: 35px;
+        height: 35px;
+        -moz-border-radius: 20px;
+        -webkit-border-radius: 20px;
+        border-radius: 20px;
+    }
+    #kreis-red {
+        background-color: #FF0000;
+        width: 35px;
+        height: 35px;
+        -moz-border-radius: 20px;
+        -webkit-border-radius: 20px;
+        border-radius: 20px;
+    }
+    textarea {
+    resize: none;
+    min-width: 267px;
+    max-width: 267px;
+    min-height:150px;
+    max-height:150px;
+    }
+</style>
